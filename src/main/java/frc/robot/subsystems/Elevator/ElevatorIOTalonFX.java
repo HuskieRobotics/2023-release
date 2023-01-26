4e1c24bd2afbafa3cbcd334498d12f85b2198a52
Aidan Donnelly <73967906+AidanDonnelly1@users.noreply.github.com>
2023-01-25 18:07:15 -0600

    added code for the elevator

---
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIOTalonFX.java
@@ -0,0 +1,238 @@
+package frc.robot.subsystems.elevator;
+
+import static frc.robot.subsystems.elevator.ElevatorConstants.*;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.Pigeon2;
+
+import frc.lib.team3061.util.CANDeviceFinder;
+import frc.lib.team3061.util.CANDeviceId.CANDeviceType;
+import frc.robot.subsystems.elevator.ElevatorIO.ElevatorIOInputs;
+
+// import frc.robot.util.CANDeviceFinder;
+// import frc.robot.util.CANDeviceId.CANDeviceType;
+
+public class ElevatorIOHardware implements ElevatorIO {
+  private boolean isControlEnabled;
+
+  private  WPI_TalonFX extensionMotor;
+  private WPI_TalonFX rotationMotor;
+
+  public void ElevatorIOTalonFX() {
+    CANDeviceFinder can = new CANDeviceFinder();
+
+    can.isDevicePresent(CANDeviceType.TALON, EXTENSION_MOTOR_CAN_ID, "Extension Motor");
+    this.extensionMotor = new WPI_TalonFX(LEFT_ELEVATOR_MOTOR_CAN_ID);
+    this.rotationMotor = new WPI_TalonFX(RIGHT_ELEVATOR_MOTOR_CAN_ID);
+
+    // the following configuration is based on the CTRE example code
+
+    /* Factory Default all hardware to prevent unexpected behaviour */
+    this.rotationMotor.configFactoryDefault();
+    this.extensionMotor.configFactoryDefault();
+
+    /** Config Objects for motor controllers */
+    TalonFXConfiguration rotationMotorConfig = new TalonFXConfiguration();
+    TalonFXConfiguration extensionMotorConfig = new TalonFXConfiguration();
+
+    /* Disable all motors */
+    this.rotationMotor.set(TalonFXControlMode.PercentOutput, 0);
+    this.extensionMotor.set(TalonFXControlMode.PercentOutput, 0);
+
+    /* Set neutral modes */
+    this.extensionMotor.setNeutralMode(NeutralMode.Brake);
+    this.rotationMotor.setNeutralMode(NeutralMode.Brake);
+    
+    this.extensionMotor.follow(this.rotationMotor);
+
+    /* Configure output */
+    this.rotationMotor.setInverted(TalonFXInvertType.Clockwise);
+    this.extensionMotor.setInverted(TalonFXInvertType.FollowMaster);
+
+    /*
+     * Talon FX does not need sensor phase set for its integrated sensor
+     * This is because it will always be correct if the selected feedback device is
+     * integrated sensor (default value)
+     * and the user calls getSelectedSensor* to get the sensor's position/velocity.
+     *
+     * https://phoenix-documentation.readthedocs.io/en/latest/ch14_MCSensor.html#
+     * sensor-phase
+     *
+     * this.extensionMotor.setSensorPhase(true)
+     * this.rotationMotor.setSensorPhase(true)
+     */
+
+    /** Feedback Sensor Configuration */
+
+    /** Distance Configs */
+
+    /* Configure the left Talon's selected sensor as integrated sensor */
+    rotationMotorConfig.primaryPID.selectedFeedbackSensor =
+        TalonFXFeedbackDevice.IntegratedSensor.toFeedbackDevice(); // Local
+    // Feedback
+    // Source
+
+    // /* FPID for Distance */
+    rotationMotorConfig.slot0.kF = POSITION_PID_F;
+    rotationMotorConfig.slot0.kP = POSITION_PID_P;
+    rotationMotorConfig.slot0.kI = POSITION_PID_I;
+    rotationMotorConfig.slot0.kD = POSITION_PID_D;
+    rotationMotorConfig.slot0.integralZone = POSITION_PID_I_ZONE;
+    rotationMotorConfig.slot0.closedLoopPeakOutput = POSITION_PID_PEAK_OUTPUT;
+
+    /* Config the neutral deadband. */
+    rotationMotorConfig.neutralDeadband = 0.001;
+
+    /**
+     * 1ms per loop. PID loop can be slowed down if need be. For example, - if sensor updates are
+     * too slow - sensor deltas are very small per update, so derivative error never gets large
+     * enough to be useful. - sensor movement is very slow causing the derivative error to be near
+     * zero.
+     */
+    int closedLoopTimeMs = 1;
+    rotationMotorConfig.slot0.closedLoopPeriod = closedLoopTimeMs;
+    rotationMotorConfig.slot1.closedLoopPeriod = closedLoopTimeMs;
+    rotationMotorConfig.slot2.closedLoopPeriod = closedLoopTimeMs;
+    rotationMotorConfig.slot3.closedLoopPeriod = closedLoopTimeMs;
+
+    /* Motion Magic Configs */
+    rotationMotorConfig.motionAcceleration =
+        ELEVATOR_ACCELERATION; // (distance units per 100 ms) per second
+    rotationMotorConfig.motionCruiseVelocity = MAX_ELEVATOR_VELOCITY; // distance units per 100 ms
+    rotationMotorConfig.motionCurveStrength = SCURVE_STRENGTH;
+
+    extensionMotorConfig.motionAcceleration =
+        ELEVATOR_ACCELERATION; // (distance units per 100 ms) per second
+    extensionMotorConfig.motionCruiseVelocity = MAX_ELEVATOR_VELOCITY; // distance units per 100 ms
+    extensionMotorConfig.motionCurveStrength = SCURVE_STRENGTH;
+
+    /* APPLY the config settings */
+    this.rotationMotor.configAllSettings(rotationMotorConfig);
+    this.extensionMotor.configAllSettings(extensionMotorConfig);
+
+    /* Initialize */
+    this.rotationMotor.getSensorCollection().setIntegratedSensorPosition(0, TIMEOUT_MS);
+
+    // these status frames aren't read; so, set these CAN frame periods to the maximum value
+    // //  to reduce traffic on the bus
+    this.extensionMotor.setStatusFramePeriod(
+        StatusFrameEnhanced.Status_1_General, 255, TIMEOUT_MS);
+    this.extensionMotor.setStatusFramePeriod(
+        StatusFrameEnhanced.Status_2_Feedback0, 255, TIMEOUT_MS);
+
+    this.pigeon = new Pigeon2(PIGEON_ID);
+  }
+
+  @Override
+  public void updateInputs(ElevatorIOInputs inputs) {
+    inputs.isControlEnabled = isControlEnabled;
+
+    inputs.extensionPosition = extensionMotor.getSelectedSensorPosition(SLOT_INDEX);
+    inputs.extensionVelocity = extensionMotor.getSelectedSensorVelocity(SLOT_INDEX);
+    inputs.extensionClosedLoopError = extensionMotor.getClosedLoopError(SLOT_INDEX);
+    inputs.extensionAppliedVolts = extensionMotor.getMotorOutputVoltage();
+    inputs.extensionCurrentAmps = new double[] {extensionMotor.getStatorCurrent()};
+    inputs.extensionTempCelcius = new double[] {extensionMotor.getTemperature()};
+
+    inputs.rotationPosition = rotationMotor.getSelectedSensorPosition(SLOT_INDEX);
+    inputs.rotationVelocity = rotationMotor.getSelectedSensorVelocity(SLOT_INDEX);
+    inputs.rotationClosedLoopError = rotationMotor.getClosedLoopError(SLOT_INDEX);
+    inputs.rotationAppliedVolts = rotationMotor.getMotorOutputVoltage();
+    inputs.rotationCurrentAmps = new double[] {rotationMotor.getStatorCurrent()};
+    inputs.rotationTempCelcius = new double[] {rotationMotor.getTemperature()};
+
+    inputs.pitch = pigeon.getPitch();
+  }
+
+  @Override
+  public void setControlEnabled(boolean controlEnabled) {
+    isControlEnabled = controlEnabled;
+  }
+
+  @Override
+  public void setExtensionMotorPercentage(double percentage) {
+    extensionMotor.set(ControlMode.PercentOutput, percentage);
+  }
+
+  @Override
+  public void setRotationMotorPercentage(double percentage) {
+    rotationMotor.set(ControlMode.PercentOutput, percentage);
+  }
+
+  @Override
+  public void setExtensionPosition(double position, double arbitraryFeedForward) {
+    extensionMotor.set(
+        TalonFXControlMode.Position,
+        position,
+        DemandType.ArbitraryFeedForward,
+        arbitraryFeedForward);
+  }
+
+  @Override
+  public void setRotationPosition(double position, double arbitraryFeedForward) {
+    rotationMotor.set(
+        TalonFXControlMode.Position,
+        position,
+        DemandType.ArbitraryFeedForward,
+        arbitraryFeedForward);
+  }
+
+  @Override
+  public void configureExtensionKF(double kF) {
+    rotationMotor.config_kF(SLOT_INDEX, kF, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configureExtensionKP(double kP) {
+    rotationMotor.config_kP(SLOT_INDEX, kP, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configureExtensionKI(double kI) {
+    rotationMotor.config_kI(SLOT_INDEX, kI, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configureExtensionKD(double kD) {
+    rotationMotor.config_kD(SLOT_INDEX, kD, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configExtensionClosedLoopPeakOutput(double peakOutput) {
+    rotationMotor.configClosedLoopPeakOutput(SLOT_INDEX, peakOutput);
+  }
+
+  
+  @Override
+  public void configureRotationKF(double kF) {
+    rotationMotor.config_kF(SLOT_INDEX, kF, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configureRotationKP(double kP) {
+    rotationMotor.config_kP(SLOT_INDEX, kP, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configureRotationKI(double kI) {
+    rotationMotor.config_kI(SLOT_INDEX, kI, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configureRotationKD(double kD) {
+    rotationMotor.config_kD(SLOT_INDEX, kD, TIMEOUT_MS);
+  }
+
+  @Override
+  public void configRotationClosedLoopPeakOutput(double peakOutput) {
+    rotationMotor.configClosedLoopPeakOutput(SLOT_INDEX, peakOutput);
+  }
+}
--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.elevator;
 
-public class ElevatorConstants {
+public class ElevatorExtensionConstants {
 
   /** Make constants for each positions */
   public static final int TOP_BLUE_POS = 0;
@@ -15,21 +15,33 @@ public class ElevatorConstants {
   public static final int MAX_HEIGHT = 0;
 
   public static final int PIGEON_ID = 18;
-  public static final int LEFT_ELEVATOR_MOTOR_CAN_ID = 22;
-  public static final int RIGHT_ELEVATOR_MOTOR_CAN_ID = 19;
+  public static final int ELEVATOR_MOTOR_CAN_ID = 22;
+  public static final int ROTATION_ELEVATOR_MOTOR_CAN_ID = 19;
   public static final int CLIMBER_CAMERA_PORT = 0;
 
-  public static final int POSITION_PID_F = 0;
-  public static final int POSITION_PID_P = 0;
-  public static final int POSITION_PID_I = 0;
-  public static final int POSITION_PID_D = 0; 
-  public static final int POSITION_PID_I_ZONE = 0;
-  public static final int POSITION_PID_PEAK_OUTPUT = 0;
+  public static final int EXTENSION_POSITION_PID_F = 0;
+  public static final int EXTENSION_POSITION_PID_P = 0;
+  public static final int EXTENSION_POSITION_PID_I = 0;
+  public static final int EXTENSION_POSITION_PID_D = 0; 
+  public static final int EXTENSION_POSITION_PID_I_ZONE = 0;
+  public static final int EXTENSION_POSITION_PID_PEAK_OUTPUT = 0;
+
+  public static final int ROTATION_POSITION_PID_F = 0;
+  public static final int ROTATION_POSITION_PID_P = 0;
+  public static final int ROTATION_POSITION_PID_I = 0;
+  public static final int ROTATION_POSITION_PID_D = 0; 
+  public static final int ROTATION_POSITION_PID_I_ZONE = 0;
+  public static final int ROTATION_POSITION_PID_PEAK_OUTPUT = 0;
   
-  public static final double SLOW_PEAK_OUTPUT = 0.15;
-  public static final double MAX_ELEVATOR_VELOCITY = 20000; // theoretical maximum 21305
-  public static final double ELEVATOR_ACCELERATION = MAX_ELEVATOR_VELOCITY * 10;
-  public static final int SCURVE_STRENGTH = 0;
+  public static final double EXTENSION_SLOW_PEAK_OUTPUT = 0.15;             // FIXME
+  public static final double EXTENSION_MAX_ELEVATOR_VELOCITY = 20000; // theoretical maximum 21305
+  public static final double EXTENSION_ELEVATOR_ACCELERATION = MAX_ELEVATOR_VELOCITY * 10;
+  public static final int EXTENSION_SCURVE_STRENGTH = 0;
+
+  public static final double ROTATION_SLOW_PEAK_OUTPUT = 0.15;             // FIXME
+  public static final double ROTATION_MAX_ELEVATOR_VELOCITY = 20000; // theoretical maximum 21305
+  public static final double ROTATION_ELEVATOR_ACCELERATION = MAX_ELEVATOR_VELOCITY * 10;
+  public static final int ROTATION_SCURVE_STRENGTH = 0;
 
   public static final int ELEVATOR_POSITION_TOLERANCE = 1000;
   public static final double ARBITRARY_FEED_FORWARD_EXTEND = .02;

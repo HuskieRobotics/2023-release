19cb2c5755d24b2d454f8765b1bdef4a7a5867f0
Geoff Schmit <gcschmit@gmail.com>
2023-02-04 23:22:04 -0600

    create triggers

--- src/main/java/frc/robot/operator_interface/DualJoysticksOI.java
+++ src/main/java/frc/robot/operator_interface/DualJoysticksOI.java
@@ -5,14 +5,13 @@
 package frc.robot.operator_interface;
 
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /** Class for controlling the robot with two Xbox controllers. */
 public class DualJoysticksOI implements OperatorInterface {
   private final CommandJoystick translateJoystick;
   private final CommandJoystick rotateJoystick;
-  public final JoystickButton[] translateJoystickButtons;
+  private final Trigger[] translateJoystickButtons;
   private final Trigger[] rotateJoystickButtons;
 
   public DualJoysticksOI(int translatePort, int rotatePort) {
@@ -21,8 +20,13 @@ public class DualJoysticksOI implements OperatorInterface {
 
     // buttons use 1-based indexing such that the index matches the button number; leave index 0 set
     // to null
-    this.translateJoystickButtons = new JoystickButton[13];
+    this.translateJoystickButtons = new Trigger[13];
     this.rotateJoystickButtons = new Trigger[13];
+
+    for (int i = 1; i < translateJoystickButtons.length; i++) {
+      translateJoystickButtons[i] = translateJoystick.button(i);
+      rotateJoystickButtons[i] = rotateJoystick.button(i);
+    }
   }
 
   @Override

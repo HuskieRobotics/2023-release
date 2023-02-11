f0b2f595601a126383747dc0a0cc66121051eb01
Jake Smith <ambase6@gmail.com>
2023-02-11 14:31:13 -0600

    Updated Pid Values

--- src/main/java/frc/robot/configs/MK4IRobotConfig.java
+++ src/main/java/frc/robot/configs/MK4IRobotConfig.java
@@ -82,9 +82,9 @@ public class MK4IRobotConfig extends RobotConfig {
   private static final double AUTO_DRIVE_P_CONTROLLER = 6.0;
   private static final double AUTO_DRIVE_I_CONTROLLER = 0.0;
   private static final double AUTO_DRIVE_D_CONTROLLER = 0.0;
-  private static final double AUTO_TURN_P_CONTROLLER = 1;
-  private static final double AUTO_TURN_I_CONTROLLER = 0.0;
-  private static final double AUTO_TURN_D_CONTROLLER = 0.0;
+  private static final double AUTO_TURN_P_CONTROLLER = 0.4;
+  private static final double AUTO_TURN_I_CONTROLLER = 1.6;
+  private static final double AUTO_TURN_D_CONTROLLER = 1.0;
 
   @Override
   public double getSwerveAngleKP() {

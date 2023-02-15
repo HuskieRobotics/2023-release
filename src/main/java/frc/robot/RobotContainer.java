ab71696152369232c3655a8528b363d97190968a
Jake Smith <92891427+Jasmith1129@users.noreply.github.com>
2023-02-14 19:48:43 -0600

    Added new path for testing drive to pose

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -475,6 +475,22 @@ public class RobotContainer {
     autoChooser.addOption(
         "Blue Loading Side 2 Cone Engage Path ( with event markers)", blueLoadingSide2ConeCommand);
 
+
+    List<PathPlannerTrajectory> driveToTag6Path =
+        PathPlanner.loadPathGroup("Drive to Tag 6", 2.0, 2.0);
+    Command driveToTag6Command =
+        Commands.sequence(
+            new FollowPath(driveToTag6Path.get(0), drivetrain, true, true),
+            new DriveToPose(drivetrain, FieldConstants.GRID_3_NODE_1),
+            Commands.runOnce(
+                () -> drivetrain.drive(-squaringSpeed.get(), 0.0, 0.0, true, true), drivetrain),
+            Commands.waitSeconds(squaringDuration.get()),
+            Commands.runOnce(drivetrain::enableXstance, drivetrain),
+            Commands.waitSeconds(2.0),
+            Commands.runOnce(drivetrain::disableXstance, drivetrain));
+    autoChooser.addOption(
+        "Drive to Pose Test Path(Tag 6)", driveToTag6Command);
+    
     // auto path for mobility bonus and preparing to engage
     PathPlannerTrajectory blueMobilityPrepareToDockPath =
         PathPlanner.loadPath("Blue-Mobility Prepare To Dock", 2.0, 2.0);

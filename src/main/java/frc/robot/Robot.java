e9ada9add1b8ca26931f9603f1bac26a0ef1e916
admodi <ambase6@gmail.com>
2023-03-01 09:32:28 -0600

    fix error in robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.lib.team6328.util.Alert;
 import frc.lib.team6328.util.Alert.AlertType;
 import frc.robot.subsystems.leds.LEDs;
-import frc.robot.subsystems.leds.LEDs.RobotStateColors;
+import frc.robot.subsystems.leds.LEDs.DriveInfoColors;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -166,7 +166,7 @@ public class Robot extends LoggedRobot {
       autonomousCommand.cancel();
     }
     led = new LEDs();
-    led.changeRobotStateColors(RobotStateColors.BLUE, RobotStateColors.RED);
+    led.changeSideStateColors(DriveInfoColors.BLUE, DriveInfoColors.YELLOW);
   }
 
   /** This method is invoked at the start of the test period. */

7fd178e776987798addec7401214058cdbabea91
Wyatt Woker <77416087+tyler51@users.noreply.github.com>
2023-03-01 20:56:57 -0600

    set min and max positions; peak output, izone, pid constants

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -70,14 +70,14 @@ public class ElevatorConstants {
   public static final double SHELF_EXTENSION_POSITION_TIME = 0;
   public static final double CHUTE_EXTENSION_POSITION_TIME = 0;
 
-  public static final int MIN_EXTENSION_POSITION = 0;
-  public static final int MAX_EXTENSION_POSITION = 0;
+  public static final double MIN_EXTENSION_POSITION = 0.0;
+  public static final double MAX_EXTENSION_POSITION = 1.722;
 
   // FIXME: change to starting position when holding a cone
   public static final int START_EXTENSION_POSITION_INCHES = 0;
 
-  public static final int MIN_ROTATION_POSITION = 0;
-  public static final int MAX_ROTATION_POSITION = 0;
+  public static final double MIN_ROTATION_POSITION = 0.0;
+  public static final double MAX_ROTATION_POSITION = 1.297;
 
   public static final int EXTENSION_ELEVATOR_MOTOR_CAN_ID = 5;
   public static final int ROTATION_ELEVATOR_MOTOR_CAN_ID = 19;
@@ -88,15 +88,15 @@ public class ElevatorConstants {
   public static final double EXTENSION_POSITION_PID_I = 0;
   public static final double EXTENSION_POSITION_PID_D = 0;
   public static final double EXTENSION_POSITION_PID_I_ZONE = 0;
-  public static final double EXTENSION_POSITION_PID_PEAK_OUTPUT = 1.0;
+  public static final double EXTENSION_POSITION_PID_PEAK_OUTPUT = 0.5;
 
   public static final boolean ROTATION_INVERTED = false;
   public static final double ROTATION_POSITION_PID_F = 0;
   public static final double ROTATION_POSITION_PID_P = 1.0;
-  public static final double ROTATION_POSITION_PID_I = 0;
+  public static final double ROTATION_POSITION_PID_I = 0.01;
   public static final double ROTATION_POSITION_PID_D = 0;
-  public static final double ROTATION_POSITION_PID_I_ZONE = 0;
-  public static final double ROTATION_POSITION_PID_PEAK_OUTPUT = .3;
+  public static final double ROTATION_POSITION_PID_I_ZONE = 0.1;
+  public static final double ROTATION_POSITION_PID_PEAK_OUTPUT = 1.0;
 
   public static final int PIGEON_ID = 4;
   public static final double EXTENSION_SLOW_PEAK_OUTPUT = 0.15;
@@ -109,13 +109,13 @@ public class ElevatorConstants {
   public static final double ROTATION_ELEVATOR_ACCELERATION = ROTATION_MAX_ELEVATOR_VELOCITY * 10;
   public static final double ROTATION_SCURVE_STRENGTH = 0;
 
-  public static final double ELEVATOR_EXTENSION_POSITION_TOLERANCE = .01;
+  public static final double ELEVATOR_EXTENSION_POSITION_TOLERANCE = .02;
   public static final double DEFAULT_EXTENSION_MOTOR_POWER = 0.5;
-  public static final double MAX_MANUAL_POWER_EXTENSION = .4;
+  public static final double MAX_MANUAL_POWER_EXTENSION = .2;
 
   public static final double ELEVATOR_ROTATION_POSITION_TOLERANCE = .01;
   public static final double DEFAULT_ROTATION_MOTOR_POWER = 0.5;
-  public static final double MAX_MANUAL_POWER_ROTATION = .4;
+  public static final double MAX_MANUAL_POWER_ROTATION = .2;
 
   public static final double EXTENSION_PULLEY_CIRCUMFERENCE = Units.inchesToMeters(1.128) * Math.PI;
   public static final double EXTENSION_GEAR_RATIO = 3.0;

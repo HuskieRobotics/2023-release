afaf0c23ed9448c0c8fd0d4c89fbf19678ad9095
Aayush Modi <ambase6@gmail.com>
2023-03-02 20:49:11 -0600

    comment out command sequence

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -825,21 +825,21 @@ public class RobotContainer {
                 () -> elevator.setElevatorRotationMotorPower(oi.getRotateArm()), elevator)));
 
     // FIXME: delete after testing
-    // oi.getResetGyroButton().onTrue(new SetElevatorPosition(elevator, armChooser));
+    oi.getResetGyroButton().onTrue(new SetElevatorPosition(elevator, armChooser));
 
     // FIXME: enable for final testing and then delete
-    oi.getResetGyroButton()
-        .onTrue(
-            Commands.either(
-                Commands.sequence(
-                    new SetElevatorPosition(elevator, armChooser),
-                    new GrabGamePiece(manipulator),
-                    new SetElevatorPosition(elevator, Position.CONE_STORAGE)),
-                Commands.sequence(
-                    new SetElevatorPosition(elevator, armChooser),
-                    new ReleaseGamePiece(manipulator),
-                    new SetElevatorPosition(elevator, Position.CONE_STORAGE)),
-                manipulator::isOpened));
+    // oi.getResetGyroButton()
+    //     .onTrue(
+    //         Commands.either(
+    //             Commands.sequence(
+    //                 new SetElevatorPosition(elevator, armChooser),
+    //                 new GrabGamePiece(manipulator),
+    //                 new SetElevatorPosition(elevator, Position.CONE_STORAGE)),
+    //             Commands.sequence(
+    //                 new SetElevatorPosition(elevator, armChooser),
+    //                 new ReleaseGamePiece(manipulator),
+    //                 new SetElevatorPosition(elevator, Position.CONE_STORAGE)),
+    //             manipulator::isOpened));
   }
 
   /**

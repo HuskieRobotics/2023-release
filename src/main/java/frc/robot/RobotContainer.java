537aa7cddd22c7e4300389e5c8f804cd23ef60b8
Geoff Schmit <gcschmit@gmail.com>
2023-03-05 11:02:53 -0600

    update score command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1114,8 +1114,9 @@ public class RobotContainer {
   private Command scoreGamePieceAuto(Position elevatorPosition) {
     Command setElevatorPositionToScoreAuto = new SetElevatorPosition(elevator, elevatorPosition);
     Command dropGamePieceAuto = new ReleaseGamePiece(manipulator);
+    Command stallOnGamePieceAuto = new GrabGamePiece(manipulator);
 
-    return Commands.sequence(setElevatorPositionToScoreAuto, dropGamePieceAuto);
+    return Commands.sequence(stallOnGamePieceAuto,setElevatorPositionToScoreAuto, dropGamePieceAuto);
   }
 
   private Command collectGamePieceAuto() {
